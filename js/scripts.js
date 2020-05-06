77b2a758c56db3200002d58b13143a37e5dd7061
martinsmandina <martinsmandina@gmail.com>
2020-05-06 13:44:42 +0300

    added if/else validation for day/month

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,22 @@ console.log(queryString);
 function getbirthday(){
 
 }
+function getdayoftheweek (){
+var dd = document.getElementById("DD").value;
+if(dd<=0 || dd>31){window.alert("Day Invalid Error")}
+else{
+var theday= var dd;
+}
+
+var mm = document.getElementById("MM").value;
+
+if(mm<=0 || mm>31){window.alert("Month Invalid Error")}
+else{
+var theday= var dd;
+}
+var yy = document.getElementById("YY").value;
+var dayofweek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+}
 
 function getakanname(){
 
@@ -12,7 +28,7 @@ var male_names = {0:"Kwasi",1:"Kwadwo",2:"Kwabena",3:"Kwaku",4:"Yaw",5:"Kofi",6:
 
 var dob=new Date("May,5, 2020");
 var normal_day = dob.getDay();
-var theday=normal_day; 
+var theday=normal_day;
 document.write("today is : "+theday);
 
 }
