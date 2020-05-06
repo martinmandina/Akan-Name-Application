5932abc79d4b31dd8c0edaf0e3672713bed1b9c2
martinsmandina <martinsmandina@gmail.com>
2020-05-06 14:17:58 +0300

    formatted results and added global array variable to html

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ var theday= var dd;
 
 var mm = document.getElementById("MM").value;
 
-if(mm<=0 || mm>31){window.alert("Month Invalid Error")}
+if(mm<=0 || mm>12){window.alert("Month Invalid Error")}
 else{
 var theday= var dd;
 }
@@ -20,7 +20,7 @@ var yy = document.getElementById("YY").value;
 var dayofweek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
 }
 
-function getakanname(){
+
 
 var female_names = {0:"Akosua",1:"Adwoa",2:"Abenaa",3:"Akua",4:"Yaa",5:"Afua",6:"Ama"};
 var male_names = {0:"Kwasi",1:"Kwadwo",2:"Kwabena",3:"Kwaku",4:"Yaw",5:"Kofi",6:"Kwame"};
@@ -31,5 +31,5 @@ var normal_day = dob.getDay();
 var theday=normal_day;
 document.write("today is : "+theday);
 
-}
+
 var akan_name=female_names[normal_day];
